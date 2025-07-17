9bc48047f8dbe8681ff92bb7ba432ad3815e8ba0
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-07-17 23:43:15 +0530

    fixing logout bug

--- client/src/Pages/users/Logout.jsx
+++ client/src/Pages/users/Logout.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-
 const Logout = () => {
     localStorage.removeItem('token'); // Clear the token from local storage
-  return (<Navigate to="/" />);
+  return (location.href = "/"); // Redirect to the home page
 }
 
 export default Logout
